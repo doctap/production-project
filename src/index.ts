2b8d8c06aeb665509c2a6f52204fa14f1681e180
doctap <vasilij.alexsandrovich@gmail.com>
2023-10-16 07:19:59 +0600

    config webpack and typescript

---
+++ src/index.ts
@@ -0,0 +1,3 @@
+import { name } from './test';
+
+name(222);
---
+++ src/test.ts
@@ -0,0 +1,5 @@
+export function name (num: number): string {
+  console.log('11111111111 ', num.toString());
+
+  return num.toString();
+}
