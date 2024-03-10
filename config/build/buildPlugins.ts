5146f37770887a028d43ac20300072241da1a2a5
doctap <vasilij.alexsandrovich@gmail.com>
2024-03-10 07:13:48 +0500

    fix webpack plugins for dev and prod

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -1,27 +1,32 @@
+import webpack from 'webpack'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
 import MiniCssExtractPlugin from 'mini-css-extract-plugin'
-import webpack, { type WebpackPluginInstance } from 'webpack'
 import ReactRefreshPlugin from '@pmmmwh/react-refresh-webpack-plugin'
 import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer'
 import type { IBuildOptions } from './types/config'
 
-export const buildPlugins = ({ paths, isDev }: IBuildOptions): webpack.WebpackPluginInstance[] => [
-  new HtmlWebpackPlugin({
-    template: paths.html,
-  }),
-  new webpack.ProgressPlugin(),
-  new MiniCssExtractPlugin({
-    filename: 'css/[name].[contenthash:8].css',
-    chunkFilename: 'css/[name].[contenthash:8].css',
-  }),
-  new webpack.DefinePlugin({
-    _IS_DEV_: JSON.stringify(isDev),
-  }),
-  new webpack.HotModuleReplacementPlugin(),
-    // clear it in filter
-    (isDev && new ReactRefreshPlugin()) as WebpackPluginInstance,
-    new BundleAnalyzerPlugin({
-      openAnalyzer: false,
+export const buildPlugins = ({ paths, isDev }: IBuildOptions): webpack.WebpackPluginInstance[] => {
+  const plugins = [
+    new HtmlWebpackPlugin({
+      template: paths.html,
     }),
-]
-  .filter(Boolean)
+    new webpack.ProgressPlugin(),
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].[contenthash:8].css',
+    }),
+    new webpack.DefinePlugin({
+      _IS_DEV_: JSON.stringify(isDev),
+    }),
+  ]
+
+  if (isDev) {
+    plugins.push(new webpack.HotModuleReplacementPlugin())
+    plugins.push(new ReactRefreshPlugin())
+    plugins.push(new BundleAnalyzerPlugin({
+      openAnalyzer: false,
+    }))
+  }
+
+  return plugins
+}
