f83d0da29783774d24f19c421dc3d787d35cab76
doctap <vasilij.alexsandrovich@gmail.com>
2024-04-08 08:56:50 +0500

    config i18n for test, fix loki sets

--- src/shared/config/i18n/i18n-configForTests.ts
+++ src/shared/config/i18n/i18n-configForTests.ts
@@ -1,20 +1,29 @@
 import i18n from 'i18next'
 import { initReactI18next } from 'react-i18next'
+import Backend from 'i18next-http-backend'
+import LanguageDetector from 'i18next-browser-languagedetector'
 
 export function initI18nextTest() {
   i18n
+    .use(Backend)
+    .use(LanguageDetector)
     .use(initReactI18next)
     .init({
       lng: 'en',
       fallbackLng: 'en',
-      debug: false,
-
+      supportedLngs: ['en'],
+      debug: true,
       interpolation: {
         escapeValue: false, // not needed for react!!
       },
-
-      resources: { en: { translations: {} } },
-    }).catch(e => {
+      react: {
+        useSuspense: false,
+      },
+      backend: {
+        loadPath: '/locales/en/{{ns}}.json',
+      },
+    })
+    .catch(e => {
       console.error(e)
     })
 }
--- src/shared/config/storybook/translationDecorator/TranslationDecorator.tsx
+++ src/shared/config/storybook/translationDecorator/TranslationDecorator.tsx
@@ -1,9 +1,11 @@
 import { Story } from '@storybook/react'
 import { I18nextProvider } from 'react-i18next'
-import i18n from 'shared/config/i18n/i18n'
+import i18n, { initI18nextTest } from 'shared/config/i18n/i18n-configForTests'
 
 export const TranslationDecorator = (Story: Story) => (
-  <I18nextProvider i18n={i18n}>
-    <Story />
-  </I18nextProvider>
+  (initI18nextTest(),
+    <I18nextProvider i18n={i18n}>
+      <Story />
+    </I18nextProvider>
+  )
 )
